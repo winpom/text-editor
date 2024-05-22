b5d098b909c121e0ff29dcc7998b2bc053381940
Tyrion Lannister <win.pomerantz@gmail.com>
2024-05-22 14:29:08 -0700

    built out README, site nearly finalized

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,8 +32,8 @@ module.exports = () => {
         description: 'Edit text!',
         background_color: '#7eb4e2',
         theme_color: '#7eb4e2',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
